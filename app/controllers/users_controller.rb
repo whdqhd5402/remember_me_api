e84405e9d1ec250c3deb1b07f00406dbf8c4980d
whdqhd5402 <whdqhd5402@gmail.com>
2017-12-04 20:31:13 +0900

    change syntax error

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,7 +17,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
 
-    ap user_paramsd
+    ap user_params
 
     if @user.save
       render json: @user, status: :created, location: @user
